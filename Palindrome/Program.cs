ea0b8ca4b57e84351adffbf7799d2db7c511b6e5
Ripal Patel <ripal212007@gmail.com>
2019-03-09 13:40:30 -0600

    working

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Palindrome
 {
@@ -6,7 +8,24 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Enter Phrase");
+            var Phrase= Console.ReadLine().ToLower();
+            var ReversePhrase = "";
+            string RemoveSpaceAndPunctuationString = Regex.Replace(Phrase, @"(?i)[^a-z]", "");
+            for (var i = RemoveSpaceAndPunctuationString.Length -1; i >= 0; i--)
+            {
+                ReversePhrase += RemoveSpaceAndPunctuationString[i].ToString();
+            }
+            if (ReversePhrase == RemoveSpaceAndPunctuationString)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+            Console.ReadKey();
+        }     
+
     }
 }
